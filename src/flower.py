8989bd05e58b697d962a5fc043a4a3b533b2911b
Matthew Simmons <mmdsimmons@gmail.com>
2024-11-24 17:36:29 -0500

    methods and stuff

---
+++ src/flower.py
@@ -0,0 +1,11 @@
+class Flower:
+    def __init__(self, x, y, img_file):
+        '''
+        initializes a flower object
+        args: 
+            x - starting x coordinate
+            y - starting y coordinate
+            img_file - path to flower image 
+        '''
+        self.x = x
+        self.y = y
---
+++ src/octopus.py
@@ -0,0 +1,39 @@
+class Octopus:
+    def __init__(self, x, y, img_file):
+        '''
+        initializes the player character/Octopus
+        args:
+            x - initial x coordinate
+            y - initial y coordinate
+            img_file - path to octopus img
+        '''
+        
+    def move_right():
+        '''
+        moves the player right by one
+        args: none
+        returns: none
+        '''
+        
+    def move_left():
+        '''
+        mooves the player left by one
+        args: none
+        returns: none
+        '''
+    
+    def move_up():
+        '''
+        moves the player up by one
+        args: none
+        returns: none
+        '''
+    
+    def move_down():
+        '''
+        moves the player down by one
+        args: none
+        returns: none
+        '''
+        
+    
